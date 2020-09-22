a7051a0ff61552de69fd4b2e492a677f1341f5ce
DiasKappassov <diaskappassov@gmail.com>
2020-09-22 08:46:29 +0600

    LF

---
+++ index.php
@@ -0,0 +1,178 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <meta name="description" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Title -->
+    <title>Document</title>
+
+    <!-- Favicon Icon -->
+    <link rel="shortcut icon" href="images/favicon.ico" type="image/png">
+
+    <!-- Bootstrap css -->
+    <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+
+    <!-- Fontawesome css -->
+    <link rel="stylesheet" href="assets/css/all.min.css">
+
+    <!-- Plugins css -->
+    <link rel="stylesheet" href="assets/css/slick.css">
+    <link rel="stylesheet" href="assets/css/nice-select.css">
+    <link rel="stylesheet" href="assets/css/magnific-popup.css">
+
+    <!-- Utilities css -->
+    <link rel="stylesheet" href="assets/css/Util.css">
+
+    <!-- Main css -->
+    <link rel="stylesheet" href="assets/css/main.css">
+
+    <!-- Responsive css -->
+    <link rel="stylesheet" href="assets/css/responsive.css">
+
+
+</head>
+
+<body>
+
+    <div class="viewport-wrapper">
+        <img src="assets/img/tt-1.png" alt="" class="transparent-img tt-1">
+        <img src="assets/img/tt-2.png" alt="" class="transparent-img tt-2">
+        <img src="assets/img/tt-3.png" alt="" class="transparent-img tt-3">
+
+
+        <!-- all popup MarkUp Start  -->
+        <!-- Popup Markup for About Menu Start  -->
+
+        <div class="modal fade" id="exampleModalAbout" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+            <div class="modal-dialog modal-dialog-centered menu-popup" role="document">
+            <div class="modal-content">
+                <div class="popup-title-wrap">
+                    <h3>About</h3>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true"><i class="fal fa-times"></i></span>
+                    </button>
+                </div>
+                <div class="modal-body">
+                    
+                    <div class="popup-description">
+                        <div class="about-m-content-wrapper">
+                            <p>Liberty Friedman, LLC is an impact-driven global macro investment manager. We aim to generate market-beating returns through investment themes across asset classes that dismantle systems of human oppression and restore our environment. </p>
+                            <h5 class="tertiary-title">Investment Thesis</h5>
+                            <p>Markets are imperfect. They reflect the values, biases, and blindnesses of all participants, especially those who control the capital. Where there is systemic bias, we see opportunity for investment, growth, and cascading returns. </p>
+
+                            <div class="pf-links">
+                                <a href="#">Chid Liberty, Managing Partner - LinkedIn</a>
+                                <a href="#">Anne Friedman, Managing Partner - LinkedIn</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            </div>
+        </div>
+        <!-- Popup Markup for About Menu End  -->
+
+        
+        <!-- Popup Markup for Funds Menu Start  -->
+        
+        <div class="modal fade" id="exampleModalFunds" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+            <div class="modal-dialog modal-dialog-centered menu-popup" role="document">
+            <div class="modal-content">
+                <div class="popup-title-wrap">
+                    <h3>Funds</h3>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true"><i class="fal fa-times"></i></span>
+                    </button>
+                </div>
+                <div class="modal-body">
+                    
+                    <div class="popup-description">
+                        <div class="fund-m-content-wrapper">
+                            <p>Liberty Friedman Fund I, LP</p>                            
+                        </div>
+                    </div>
+                </div>
+            </div>
+            </div>
+        </div>
+        <!-- Popup Markup for Funds Menu End  -->
+
+        
+        <!-- Popup Markup for Contact Menu Start  -->
+        
+        <div class="modal fade" id="exampleModalContact" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+            <div class="modal-dialog modal-dialog-centered menu-popup" role="document">
+            <div class="modal-content">
+                <div class="popup-title-wrap">
+                    <h3>Contact</h3>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true"><i class="fal fa-times"></i></span>
+                    </button>
+                </div>
+                <div class="modal-body">
+                    
+                    <div class="popup-description">
+                        <form action="#">
+                            <div class="input-inside">
+                                <input type="text" placeholder="Name">
+                            </div>
+                            <div class="input-inside">
+                                <input type="text" placeholder="Email">
+                            </div>
+                            <div class="input-inside">
+                                <input type="text" placeholder="Subject">
+                            </div>
+                            <div class="input-inside">
+                                <textarea name="comment" id="comment" cols="30" rows="10" placeholder="Comment"></textarea>
+                            </div>
+                            <div class="submit-button">
+                                <button type="submit">Submit</button>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+            </div>
+        </div>
+        <!-- Popup Markup for Contact Menu End  -->
+        <!-- all popup MarkUp End -->
+
+
+        <div class="viewport-content-box">
+            <h1 class="viewport-title">an impact-driven global macro hedge fund</h1>
+            <div class="door-img">
+                <img src="assets/img/door.png" alt="">
+            </div>
+            <div class="viewport-btm-content">
+                <ul class="main-menu">
+                    <li><a href="#" data-toggle="modal" data-target="#exampleModalAbout">About</a></li>
+                    <li><a href="#" data-toggle="modal" data-target="#exampleModalFunds">Funds</a></li>
+                    <li><a href="#" data-toggle="modal" data-target="#exampleModalContact">Contact</a></li>
+                </ul>
+            </div>
+        </div>
+    </div> <!-- ./viewport-wrapper -->
+
+    <!-- Jquery Js -->
+    <script src="assets/js/vendor/modernizr-3.6.0.min.js"></script>
+    <script src="assets/js/vendor/jquery-1.12.4.min.js"></script>
+
+    <!-- Bootstrap Js -->
+    <script src="assets/js/bootstrap.min.js"></script>
+
+    <!-- Plugins Js -->
+    <script src="assets/js/slick.min.js"></script>
+    <script src="assets/js/jquery.nice-select.min.js"></script>
+    <script src="assets/js/jquery.magnific-popup.min.js"></script>
+
+    <!-- Main Js -->
+    <script src="assets/js/main.js"></script>
+
+</body>
+
+</html>
